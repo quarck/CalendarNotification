20d8f3f30b331a107951d564d7dba62b7e69fb9d
Sergey Parshin <quarck@gmail.com>
2016-03-07 16:49:06 +0000

    Simplified / cleaned up logging

--- app/src/main/java/com/github/quarck/calnotify/logs/Logger.kt
+++ app/src/main/java/com/github/quarck/calnotify/logs/Logger.kt
@@ -22,17 +22,10 @@ package com.github.quarck.calnotify.logs
 import android.util.Log
 
 class Logger(val tag: String) {
-    private val TAG_PREFIX = "CalendarNotification:"
 
-    fun debug(message: String) {
-        Log.d(TAG_PREFIX + tag, "" + System.currentTimeMillis() + " " + message)
-    }
+    fun debug(message: String) = Log.d(tag, message)
 
-    fun info(message: String) {
-        Log.i(TAG_PREFIX + tag, "" + System.currentTimeMillis() + " " + message)
-    }
+    fun info(message: String) = Log.i(tag, message)
 
-    fun error(message: String) {
-        Log.e(TAG_PREFIX + tag, "" + System.currentTimeMillis() + " " + message)
-    }
+    fun error(message: String) = Log.e(tag, message)
 }

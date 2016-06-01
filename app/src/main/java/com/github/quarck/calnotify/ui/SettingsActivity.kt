ebbe6d51122e84d015ed139b40b72b5e00390a9d
Sergey Parshin <quarck@gmail.com>
2016-06-01 17:26:12 +0100

    Hacky fix for #69

--- app/src/main/java/com/github/quarck/calnotify/ui/SettingsActivity.kt
+++ app/src/main/java/com/github/quarck/calnotify/ui/SettingsActivity.kt
@@ -21,12 +21,67 @@ package com.github.quarck.calnotify.ui
 
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.preference.PreferenceScreen
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.github.quarck.calnotify.R
 
 class SettingsActivity : PreferenceActivity() {
     @SuppressWarnings("deprecation")
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.preferences)
+
+        for (nestedPref in nestedPreferences) {
+            val preferenceScreen = findPreference(nestedPref) as PreferenceScreen
+
+            preferenceScreen.setOnPreferenceClickListener({
+                preference ->
+
+                preferenceScreen.dialog.actionBar.setDisplayHomeAsUpEnabled(true);
+
+                val dialog = preferenceScreen.dialog;
+
+                val homeBtn = dialog.findViewById(android.R.id.home);
+                if (homeBtn != null) {
+                    val dismissListener = View.OnClickListener { dialog.dismiss(); }
+
+                    // Prepare yourselves for some hacky programming
+                    val homeBtnContainer = homeBtn.parent;
+
+                    // The home button is an ImageView inside a FrameLayout
+                    if (homeBtnContainer is FrameLayout) {
+                        val containerParent = homeBtnContainer.parent as ViewGroup
+
+                        if (containerParent is LinearLayout) {
+                            // This view also contains the title text, set the whole view as clickable
+                            containerParent.setOnClickListener(dismissListener);
+                        } else {
+                            // Just set it on the home button
+                            homeBtnContainer.setOnClickListener(dismissListener);
+                        }
+                    } else {
+                        // The 'If all else fails' default case
+                        homeBtn.setOnClickListener(dismissListener);
+                    }
+                }
+                true
+            })
+        }
+    }
+
+    companion object {
+        val nestedPreferences =
+            listOf(
+                "pref_key_notification_settings",
+                "pref_key_notification_settings",
+                "pref_key_snooze_prs",
+                "pref_key_reminder",
+                "pref_key_quiet_hours",
+                "pref_key_smartwatch",
+                "pref_key_misc" )
     }
 }

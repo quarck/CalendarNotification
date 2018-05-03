cc3cf3a78b782ed5759320eb9237653a5689aab0
Sergey Parshin <quarck@gmail.com>
2018-05-03 01:04:33 +0100

    Bugfixes

--- app/src/main/java/com/github/quarck/calnotify/notification/EventNotificationManager.kt
+++ app/src/main/java/com/github/quarck/calnotify/notification/EventNotificationManager.kt
@@ -458,6 +458,8 @@ class EventNotificationManager : EventNotificationManagerInterface {
                         .setStyle(NotificationCompat.BigTextStyle().bigText(bigText))
                         .setNumber(numEvents)
                         .setShowWhen(false)
+                        .setOnlyAlertOnce(true)
+
 
         if (shouldPlayAndVibrate) {
             if (notificationsSettings.ringtoneUri != null
@@ -920,6 +922,7 @@ class EventNotificationManager : EventNotificationManagerInterface {
                 .setWhen(System.currentTimeMillis())
                 .setShowWhen(false)
                 .setNumber(numTotalEvents)
+                .setOnlyAlertOnce(true)
 
         if (numTotalEvents > 1) {
             groupBuilder.setSmallIcon(R.drawable.stat_notify_calendar_multiple)
@@ -1059,6 +1062,9 @@ class EventNotificationManager : EventNotificationManagerInterface {
                 )
                 .setCategory(
                         NotificationCompat.CATEGORY_EVENT
+                )
+                .setOnlyAlertOnce(
+                        true
                 )
 
         builder.setGroup(NOTIFICATION_GROUP)
@@ -1395,6 +1401,8 @@ class EventNotificationManager : EventNotificationManagerInterface {
                         .setOngoing(true)
                         .setStyle(NotificationCompat.BigTextStyle().bigText(bigText))
                         .setShowWhen(false)
+                        .setOnlyAlertOnce(true)
+
 
         builder.setGroup(NOTIFICATION_GROUP)
 
@@ -1432,6 +1440,7 @@ class EventNotificationManager : EventNotificationManagerInterface {
                 .setShowWhen(false)
                 .setCategory(NotificationCompat.CATEGORY_ERROR)
                 .setLights(Consts.DEFAULT_LED_COLOR, Consts.LED_DURATION_ON, Consts.LED_DURATION_OFF)
+                .setOnlyAlertOnce(true)
 
         val notification = builder.build()
 
--- app/src/main/java/com/github/quarck/calnotify/notification/NotificationChannelManager.kt
+++ app/src/main/java/com/github/quarck/calnotify/notification/NotificationChannelManager.kt
@@ -23,6 +23,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.media.AudioAttributes
+import android.media.AudioManager
 import com.github.quarck.calnotify.NotificationSettingsSnapshot
 import com.github.quarck.calnotify.R
 import com.github.quarck.calnotify.Settings
@@ -48,8 +49,6 @@ object NotificationChannelManager {
     const val NOTIFICATION_CHANNEL_ID_SILENT = "com.github.calnotify.notify.quiet"
     const val NOTIFICATION_CHANNEL_ID_REMINDER = "com.github.calnotify.notify.reminder"
     const val NOTIFICAITON_CHANNEL_ID_REMINDER_ALARM = "com.github.calnotify.notify.ralarm"
-
-    val createdChannels = hashMapOf<NotificationChannelPurpose, String>()
 
     fun createDefaultNotificationChannel(context: Context): String {
 
@@ -84,99 +83,123 @@ object NotificationChannelManager {
 
         return channelId
     }
-//
-//    fun createNotificationChannel(
-//            context: Context,
-//            notificationPurpose: NotificationChannelPurpose
-//    ): String {
-//
-//        if (notificationPurpose in createdChannels) {
-//            return createdChannels[notificationPurpose]
-//        }
-//
-//        val channelName: Int
-//        val channelDesc: Int
-//        val channelId: String
-//
-//        when (notificationSettings.ringtoneOrigin) {
-//            RingtoneOrigin.Main -> {
-//                if (isAlarmStream) {
-//                    channelName = R.string.channel_main_alarm_name
-//                    channelDesc = R.string.channel_main_alarm_desc
-//                    channelId = Consts.NOTIFICAITON_CHANNEL_ID_MAIN_ALARM
-//                } else {
-//                    channelName = R.string.channel_main_name
-//                    channelDesc = R.string.channel_main_desc
-//                    channelId = Consts.NOTIFICATION_CHANNEL_ID_MAIN
-//                }
-//            }
-//            RingtoneOrigin.Reminder -> {
-//                if (isAlarmStream) {
-//                    channelName = R.string.channel_reminder_alarm_name
-//                    channelDesc = R.string.channel_reminder_alarm_desc
-//                    channelId = Consts.NOTIFICATION_CHANNEL_ID_ALARM_REMINDER
-//                } else {
-//                    channelName = R.string.channel_reminder_name
-//                    channelDesc = R.string.channel_reminder_desc
-//                    channelId = Consts.NOTIFICATION_CHANNEL_ID_REMINDER
-//                }
-//            }
-//            RingtoneOrigin.Quiet -> {
-//                channelName = R.string.channel_quiet_name
-//                channelDesc = R.string.channel_quiet_desc
-//                channelId = Consts.NOTIFICATION_CHANNEL_ID_QUIET
-//            }
-//        }
-//
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
-//            return channelId
-//
-//        val notificationChannel =
-//                NotificationChannel(
-//                        channelId,
-//                        context.resources.getString(channelName),
-//                        if (isAlarmStream)
-//                            NotificationManager.IMPORTANCE_HIGH
-//                        else
-//                            NotificationManager.IMPORTANCE_DEFAULT
-//                )
-//
-//        // Configure the notification channel.
-//        notificationChannel.description = context.resources.getString(channelDesc)
-//
-//        if (notificationSettings.ledNotificationOn) {
-//            notificationChannel.enableLights(true)
-//            notificationChannel.lightColor = notificationSettings.ledColor
-//        }
-//        else {
-//            notificationChannel.enableLights(false)
-//        }
-//
-//        if (notificationSettings.vibrationOn) {
-//            notificationChannel.enableVibration(true)
-//            notificationChannel.vibrationPattern = notificationSettings.vibrationPattern
-//        }
-//        else  {
-//            notificationChannel.enableVibration(false)
-//        }
-//
-//        val attribBuilder = AudioAttributes.Builder()
-//                .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
-//
-//        if (isAlarmStream) {
-//            attribBuilder
-//                    .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
-//                    .setLegacyStreamType(AudioManager.STREAM_ALARM)
-//                    .setUsage(AudioAttributes.USAGE_ALARM)
-//        }
-//        else {
-//            attribBuilder.setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT)
-//        }
-//
-//        notificationChannel.setSound(notificationSettings.ringtoneUri, attribBuilder.build())
-//
-//        context.notificationManager.createNotificationChannel(notificationChannel)
-//
-//        return channelId
-//    }
+
+    fun createNotificationChannel(
+            context: Context,
+            notificationPurpose: NotificationChannelPurpose
+    ): String {
+
+        val channelId: String
+        val channelName: String
+        val channelDesc: String
+
+        var isAlarm = false
+        var isSilent = false
+        var isReminder = false
+
+        val settings = Settings(context)
+
+        when (notificationPurpose) {
+
+            NotificationChannelPurpose.RegularNotification -> {
+                channelId = NOTIFICATION_CHANNEL_ID_DEFAULT
+                channelName = context.getString(R.string.notification_channel_default)
+                channelDesc = context.getString(R.string.notification_channel_default_desc)
+            }
+
+            NotificationChannelPurpose.RegularAlarmNotification -> {
+                channelId = NOTIFICATION_CHANNEL_ID_ALARM
+                channelName = context.getString(R.string.notification_channel_alarm)
+                channelDesc = context.getString(R.string.notification_channel_alarm_desc)
+                isAlarm = true
+            }
+
+            NotificationChannelPurpose.RegularSilentNotification -> {
+                channelId = NOTIFICATION_CHANNEL_ID_SILENT
+                channelName = context.getString(R.string.notification_channel_silent)
+                channelDesc = context.getString(R.string.notification_channel_silent_desc)
+                isSilent = true
+            }
+
+            NotificationChannelPurpose.ReminderNotification -> {
+                channelId = NOTIFICATION_CHANNEL_ID_REMINDER
+                channelName = context.getString(R.string.notification_channel_reminders)
+                channelDesc = context.getString(R.string.notification_channel_reminders_desc)
+                isReminder = true
+            }
+
+            NotificationChannelPurpose.ReminderAlarmNotification -> {
+                channelId = NOTIFICAITON_CHANNEL_ID_REMINDER_ALARM
+                channelName = context.getString(R.string.notification_channel_alarm_reminders)
+                channelDesc = context.getString(R.string.notification_channel_alarm_reminders_desc)
+                isAlarm = true
+                isReminder = true
+            }
+        }
+
+        val notificationChannel =
+                NotificationChannel(
+                        channelId,
+                        channelName,
+                        if (isAlarm)
+                            NotificationManager.IMPORTANCE_HIGH
+                        else
+                            NotificationManager.IMPORTANCE_DEFAULT
+                )
+
+        // Configure the notification channel.
+        notificationChannel.description = channelDesc
+
+        if (settings.ledNotificationOn) {
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = settings.ledColor
+        }
+        else {
+            notificationChannel.enableLights(false)
+        }
+
+
+        if (!isSilent) {
+            if (!isReminder) {
+                if (settings.vibraOn) {
+                    notificationChannel.enableVibration(true)
+                    notificationChannel.vibrationPattern = settings.vibrationPattern
+                } else {
+                    notificationChannel.enableVibration(false)
+                }
+
+            } else {
+                if (settings.reminderVibraOn) {
+                    notificationChannel.enableVibration(true)
+                    notificationChannel.vibrationPattern = settings.reminderVibrationPattern
+                } else {
+                    notificationChannel.enableVibration(false)
+                }
+            }
+        }
+
+        val attribBuilder = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
+
+        if (isAlarm) {
+            attribBuilder
+                    .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+                    .setLegacyStreamType(AudioManager.STREAM_ALARM)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+        }
+        else {
+            attribBuilder.setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT)
+        }
+
+        if (!isSilent) {
+            if (!isReminder)
+                notificationChannel.setSound(settings.ringtoneURI, attribBuilder.build())
+            else
+                notificationChannel.setSound(settings.reminderRingtoneURI, attribBuilder.build())
+        }
+
+        context.notificationManager.createNotificationChannel(notificationChannel)
+
+        return channelId
+    }
 }
